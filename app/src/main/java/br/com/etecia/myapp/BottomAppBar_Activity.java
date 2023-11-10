a3eea7c8096c940ce43e0ec830a26c18ba92cb0c
ArthurGSO <arthurprobr422@gmail.com>
2023-11-09 22:06:47 -0300

    second

---
+++ app/src/main/java/br/com/etecia/myapp/BottomAppBar_Activity.java
@@ -0,0 +1,14 @@
+package br.com.etecia.myapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BottomAppBar_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_bottom_app_bar);
+    }
+}
